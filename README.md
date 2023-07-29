0c5ada4c409a9e9cb7baecfb6149bd8be7f9c0df
simon gao <little51@126.com>
2023-07-29 23:44:34 +0800

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,111 @@
-# Llama2-chinese
-Llama2 chinese finetun
+# LLaMA2中文微调
+
+LLaMA2模型推出后，LLaMA2-Chat也同时推出，本人在16G推理卡上实践了微调Llama-2-7b-chat（ https://zhuanlan.zhihu.com/p/645152512 ，代码在 https://github.com/git-cloner/llama2-lora-fine-tuning ），但即使扩充了中文词表，推理效果依然不佳，回答主要以英文为主。
+
+其实官方在LLaMA2模型推出时，就已推出了官方的微调程序，叫做LLaMA伴侣（ https://github.com/facebookresearch/llama-recipes ）。
+
+本文是在llama-recipes的基础上，修改适配显卡资源，基于Lora的微调实践，效果尚可，并给出来测试过程和流式接口。
+
+## 1、显卡要求
+
+16G及以上，最好有两块以上。
+
+100多M的语料，在两块P100（16G）上微调一轮需要120小时。
+
+## 2、微调过程
+
+### 2.1 下载代码
+
+```bash
+git clone https://github.com/git-cloner/Llama2-chinese
+cd Llama2-chinese
+```
+
+### 2.2 安装虚拟环境
+
+```bash
+conda create -n llama-recipes python=3.9 -y
+conda activate llama-recipes
+# 因为requirements中有从github中安装的依赖，网络环境不佳，打开这两个参数可以观察进度
+export GIT_TRACE=1
+export GIT_CURL_VERBOSE=1
+pip install -r requirements.txt -i https://pypi.mirrors.ustc.edu.cn/simple
+```
+
+### 2.3 下载Llama2-7b原始模型
+
+```bash
+# 用本项目开发的下载器下载模型，可以断点续传和重连
+python model_download.py --repo_id NousResearch/Llama-2-7b-hf
+# 下载后的模型在 ./models\NousResearch\Llama-2-7b-hf 下
+```
+
+### 2.4 语料准备
+
+语料采用了alpaca格式（huggingface.co中alpaca语料很多，可自行整理），个性化修改后，放到ft_datasets/alpaca_data.json
+
+### 2.5 微调过程
+
+
+```bash
+# kill process force
+pkill -9 -f llama_finetuning
+# train，batch_size_training可按显存大小反复试，尽量把显存占满
+# 本例是用两个P100，分别是第1、2块
+# 注意nproc_per_node是1，不是2
+CUDA_VISIBLE_DEVICES=1,2 nohup torchrun --nnodes 1 --nproc_per_node 1   \
+llama_finetuning.py \
+--use_peft \
+--peft_method lora \
+--model_name ./models/NousResearch/Llama-2-7b-hf \
+--use_fp16 \
+--output_dir output/model \
+--dataset alpaca_dataset \
+--batch_size_training 40 \
+--num_epochs 3 \
+--quantization > train.log  2>&1 &
+# check log
+tail -f train.log
+```
+
+## 3、推理测试
+
+微调一轮后，会产生peft增量模型，在output/model下，用以下命令在客户端交互测试。由于未采用流模式，一次性生成后，才能看到结果，所以较慢。
+
+```bash
+CUDA_VISIBLE_DEVICES=0 python generate.py \
+    --base_model './models/NousResearch/Llama-2-7b-hf' \
+    --lora_weights './output/model' \
+    --load_8bit 
+```
+
+## 4、流式API测试
+
+### 4.1 开启API服务
+
+```bash
+# 可以用4bit或8bit量化方式装入模型测试，其中4bit需要约6G显存，8bit需要9G显存，半精度需要13G显存
+CUDA_VISIBLE_DEVICES=0 python api_stream.py \
+# --load_8bit
+ --load_4bit
+```
+
+### 4.2 测试
+
+```bash
+# 多次发POST请求，直到返回的response中包含[stop]后停止调用
+curl -X POST "http://127.0.0.1:8000/stream" \
+     -H 'Content-Type: application/json' \
+     -d '{"prompt": "你好", "history": []}'
+```
+
+## 5、模型合并
+
+```bash
+python inference/hf-text-generation-inference/merge_lora_weights.py \
+--base_model ./models/NousResearch/Llama-2-7b-hf \
+--peft_model output/model \
+--output_dir output/merged_model_output
+```
+
+
---
+++ api_stream.py
@@ -0,0 +1,201 @@
+import os
+import sys
+import time
+import json
+import datetime
+
+import argparse
+import torch
+import transformers
+from peft import PeftModel
+from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer, TextIteratorStreamer, BitsAndBytesConfig
+from threading import Thread
+from utils.prompter import Prompter
+from fastapi import FastAPI, Request
+import uvicorn
+
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+
+prompter = Prompter("")
+model = None
+tokenizer = None
+stream_buffer = {}
+app = FastAPI()
+
+
+def torch_gc():
+    if torch.cuda.is_available():
+        with torch.cuda.device(device):
+            torch.cuda.empty_cache()
+            torch.cuda.ipc_collect()
+
+
+def removeTimeoutBuffer():
+    global stream_buffer
+    for key in stream_buffer.copy():
+        diff = datetime.datetime.now() - stream_buffer[key]["time"]
+        seconds = diff.total_seconds()
+        print(key + ": 已存在" + str(seconds) + "秒")
+        if seconds > 120:
+            if stream_buffer[key]["stop"]:
+                del stream_buffer[key]
+                print(key + "：已被从缓存中移除")
+            else:
+                stream_buffer[key]["stop"] = True
+                print(key + "：已被标识为结束")
+
+
+def stream_item(
+    instruction,
+    input=None,
+    temperature=0.1,
+    top_p=0.75,
+    top_k=40,
+    num_beams=1,
+    max_new_tokens=128,
+    **kwargs,
+):
+    global model
+    global tokenizer
+    global stream_buffer
+
+    _prompt = prompter.generate_prompt(instruction, input)
+    inputs = tokenizer(_prompt, return_tensors="pt")
+    input_ids = inputs["input_ids"].to(device)
+    generation_config = GenerationConfig(
+        temperature=temperature,
+        top_p=top_p,
+        top_k=top_k,
+        num_beams=num_beams,
+        repetition_penalty=2.0,
+        **kwargs,
+    )
+
+    streamer = TextIteratorStreamer(
+        tokenizer, timeout=10.0, skip_prompt=True, skip_special_tokens=True
+    )
+    generate_kwargs = dict(
+        input_ids=input_ids,
+        generation_config=generation_config,
+        return_dict_in_generate=True,
+        output_scores=True,
+        max_new_tokens=max_new_tokens,
+        streamer=streamer,
+    )
+    t1 = Thread(target=model.generate, kwargs=generate_kwargs)
+    t1.start()
+
+    response = ""
+    for new_text in streamer:
+        response += new_text
+        time.sleep(0.05)
+        now = datetime.datetime.now()
+        print(response)
+        stream_buffer[instruction] = {
+            "response": response, "stop": False, "history": [], "time": now}
+    stream_buffer[instruction]["stop"] = True
+    torch_gc()
+    return "OK"
+
+
+def LoadModel(
+    load_4bit: bool = False,
+    load_8bit: bool = False,
+    base_model: str = "",
+    lora_weights: str = ""
+):
+    global model
+    global tokenizer
+    if load_4bit:
+        load_8bit = False
+    tokenizer = LlamaTokenizer.from_pretrained(base_model)
+    if device == "cuda":
+        model = LlamaForCausalLM.from_pretrained(
+            base_model,
+            load_in_8bit=load_8bit,
+            load_in_4bit=load_4bit,
+            torch_dtype=torch.float16,
+            device_map="auto"
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            lora_weights,
+            torch_dtype=torch.float16,
+        )
+    else:
+        model = LlamaForCausalLM.from_pretrained(
+            base_model, device_map={"": device}, low_cpu_mem_usage=True
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            lora_weights,
+            device_map={"": device},
+        )
+
+    model.config.pad_token_id = tokenizer.pad_token_id = 0
+    model.config.bos_token_id = 1
+    model.config.eos_token_id = 2
+
+    if not load_8bit:
+        model.half()
+
+    model.eval()
+    if torch.__version__ >= "2" and sys.platform != "win32":
+        model = torch.compile(model)
+
+
+@app.post("/stream")
+async def create_item(request: Request):
+    global stream_buffer
+    # remove timeout buffer
+    removeTimeoutBuffer()
+    # get request
+    json_post_raw = await request.json()
+    json_post = json.dumps(json_post_raw)
+    json_post_list = json.loads(json_post)
+    prompt = json_post_list.get('prompt')
+    history = json_post_list.get('history')
+    # new thread
+    now = datetime.datetime.now()
+    if stream_buffer.get(prompt) is None:
+        stream_buffer[prompt] = {"response": "",
+                                 "stop": False, "history": [], "time": now}
+        sub_thread = Thread(target=stream_item, args=(prompt,))
+        sub_thread.start()
+    # return soon
+    time = now.strftime("%Y-%m-%d %H:%M:%S")
+    response = stream_buffer[prompt]["response"]
+    history = stream_buffer[prompt]["history"]
+    # stop flag
+    if stream_buffer[prompt]["stop"]:
+        response = response + '[stop]'
+    answer = {
+        "response": response,
+        "history": history,
+        "status": 200,
+        "time": time
+    }
+    log = "[" + time + "] " + '", prompt:"' + \
+        prompt + '", response:"' + repr(response) + '"'
+    print(log)
+
+    return answer
+
+if __name__ == "__main__":
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--base_model', default="./models/NousResearch/Llama-2-7b-hf", type=str)
+    parser.add_argument('--lora_weights', default="./output/model", type=str,
+                        help="If None, perform inference on the base model")
+    parser.add_argument('--load_8bit', action='store_true',
+                        help='only use CPU for inference')
+    parser.add_argument('--load_4bit', action='store_true',
+                        help='only use CPU for inference')
+    args = parser.parse_args()
+    LoadModel(args.load_4bit, args.load_8bit,
+              args.base_model, args.lora_weights)
+    uvicorn.run(app, host='0.0.0.0', port=8000, workers=1)
